476f2781783b53a101782c716ef85204547d40f2
ShariShah1234 <muhammadshaheryar74@gmail.com>
2022-11-09 21:21:35 +0500

    COMMIT

---
+++ app.js
@@ -0,0 +1,80 @@
+// var fruit1 = "orange";
+// var fruit2 = "apple";
+// var fruit3 = "banana";
+// var fruit4 = "mango";
+// var fruit5 = "amma";
+
+// var a = ["orange","apple","banana","mango","amma"];
+// a[1] = "ABC";
+//  a[5] = "apple";
+//  a[6] = "graphes";
+// // a[10] = "apple";
+// console.log(a);
+
+
+// push array ka last ma array add krna. 
+// pop array ka last sa ak value delete krna.
+// a.push("banana");
+// console.log(a);
+
+// a.pop("banana");
+// console.log(a);
+
+
+
+// shift :array ka start sa ak value add krngy 
+// unshift: array ka start sa ak  value remove krngy
+// a.shift("orange");
+// console.log(a);
+// a.unshift("orange");
+// console.log(a);
+
+
+// slice  ma copy hota ha Array
+// splice array sa koi value out krna ha 
+// a.splice(1,3);
+// console.log(a);
+
+
+// a.slice(0);
+// console.log(a);
+
+// var b = a.splice(3,1)
+// console.log(a);
+
+
+// loop
+// var a =[];
+// for(var i=0; i<10;i++){
+//     a.push ("abc"+ i)
+   
+// }
+// console.log(a);
+
+
+
+// var a =["abc","amk","ikj","jhg","smh"]
+// for(var i=1;i<a.length;i++){
+//     console.log(a[i]);
+// }
+
+
+// var a =["amk","ikj","jhg","smh","abc"]
+
+
+
+// for(var i=1;i<a.length;i++){
+
+//   console.log(a[i]);
+//   if("abc"==a[i]);
+//   alert("found")
+//   break;
+// }
+
+
+
+// for (var i = 0; i <= 4; i++) {
+//      if (cityToCheck === pakistanCities[i]) {
+//      alert("It's one of the cleanest cities");
+//      }
+//  }
